28c684865928ee7400e984362e76e42d112a4a3c
Urom Jehoshaphat <jehoshaphaturom@gmail.com>
2025-06-25 17:32:53 +0100

    Fix errors

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
 # create a file in tmp directory
 file { '/tmp/school':
-	ensure => 'file',
-	content => "I love Puppet"
-	mode => '0744'
-	group => 'www-data',
-	owner => 'www-data'
+  ensure  => 'file',
+  content => 'I love Puppet',
+  mode    => '0744',
+  group   => 'www-data',
+  owner   => 'www-data'
 }
