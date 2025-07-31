1437ebf905520e408dcf280b0b4a8caa37e7d90d
Urom Jehoshaphat <jehoshaphaturom@gmail.com>
2025-07-31 17:11:43 +0100

    export some info to a csv file

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+""" Python script that uses REST API  to return info about an
+    employee's TODO list progress
+"""
+from requests import get
+from sys import argv
+import csv
+
+if __name__ == '__main__':
+    emp_id = int(argv[1])
+
+    response = get('https://jsonplaceholder.typicode.com/todos/')
+    real = response.json()
+
+    lister = []
+    for item in real:
+        if item['userId'] == emp_id:
+            lister.append(item)
+
+    resp_1 = get('https://jsonplaceholder.typicode.com/users')
+    real_2 = resp_1.json()
+    for item in real_2:
+        if item['id'] == emp_id:
+            emp_name = item['username']
+            break
+
+    new_list = []
+    for item in lister:
+        my_list = [emp_id, emp_name, item['completed'], item['title']]
+        new_list.append(my_list)
+
+    with open("2.csv", mode="w", encoding="utf-8") as file:
+        writer = csv.writer(file, quoting=csv.QUOTE_ALL)
+        writer.writerows(new_list)
